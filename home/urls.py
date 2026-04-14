6e1ba8e3c85bfbdf8a8b9b683f5125f25f0a1922
Hiram <hscobuci@gmail.com>
2026-04-14 10:05:06 -0300

    feat: implementar template e ajustar acessos

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.main_page, name='home')
+    path('', views.main_page, name='inicio'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('blog/', views.blog, name='blog'),
+    path('contato/', views.contato, name='contato'),
+    path('agendar/', views.agendar_consulta, name='agendar_consulta')
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
 def main_page(request):
-    return render(request, '')
+    return render(request, 'home/home.html')
+
+def sobre(request):
+    return render(request, 'home/sobre.html')
+
+def blog(request):
+    return render(request, 'home/blog.html')
+
+def contato(request):
+    return render(request, 'home/contato.html')
+
+def agendar_consulta(request):
+    return render(request, 'home/agendar_consulta.html')
 
 
