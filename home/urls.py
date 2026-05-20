f9986a035df2eb8bf0e6ddd0a3355109874796cc
Hiram <hscobuci@gmail.com>
2026-05-20 19:43:52 -0300

    feat: melhora navegação entre páginas e ajustes gerais de design

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.home, name='inicio'),
+    path('', views.home, name='home'),
     path('para_pais/', views.para_pais, name='para_pais'),
     path('agendar/', views.agendar, name='agendar_consulta')
 ]
