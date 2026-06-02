c9d2cd5f1338d6ec09cdf0ff1eef8e8ceafdbe33
Hiram <hscobuci@gmail.com>
2026-06-02 09:23:09 -0300

    fix: reorganiza head e configura STATICFILES_DIRS para mapear estáticos da home

--- core/settings.py
+++ core/settings.py
@@ -108,5 +108,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'home' / 'static',
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
